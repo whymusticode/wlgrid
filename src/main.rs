7e4bb30249d581d7f9ed70f6cd4dc2a405362b13
Mike Benton <billgat48@proton.me>
2026-03-16 19:54:57 -0400

    Asdf

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,61 @@
-use std::{num::NonZeroU32, sync::Arc};
-use softbuffer::{Context, Surface};
-use winit::{application::ApplicationHandler, event::WindowEvent, event_loop::{ActiveEventLoop, EventLoop}, raw_window_handle::HasDisplayHandle, window::Window};
+// Minimal Hyprland/Wayland render test: winit + softbuffer (no wgpu).
+use std::num::NonZeroU32;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::window::WindowAttributes;
 
-struct App { c: Option<Context<winit::raw_window_handle::DisplayHandle<'static>>>, w: Option<Arc<Window>>, s: Option<Surface<winit::raw_window_handle::DisplayHandle<'static>, Arc<Window>>> }
-impl ApplicationHandler for App {
-    fn resumed(&mut self, e: &ActiveEventLoop) { let w = Arc::new(e.create_window(Window::default_attributes().with_title("wlgrid")).unwrap()); let c = Context::new(unsafe { std::mem::transmute(e.display_handle().unwrap()) }).unwrap(); let s = Surface::new(&c, w.clone()).unwrap(); self.c = Some(c); self.s = Some(s); self.w = Some(w); self.w.as_ref().unwrap().request_redraw(); }
-    fn window_event(&mut self, e: &ActiveEventLoop, _: winit::window::WindowId, ev: WindowEvent) { match ev { WindowEvent::CloseRequested => e.exit(), WindowEvent::Resized(z) => if let (Some(w), Some(h), Some(s)) = (NonZeroU32::new(z.width), NonZeroU32::new(z.height), self.s.as_mut()) { s.resize(w, h).unwrap(); self.w.as_ref().unwrap().request_redraw(); }, WindowEvent::RedrawRequested => if let Some(s) = self.s.as_mut() { let mut b = s.buffer_mut().unwrap(); for (i, p) in b.iter_mut().enumerate() { let x = i as u32 % b.width(); let y = i as u32 / b.width(); *p = (x ^ y) | 0x00ff00; } self.w.as_ref().unwrap().pre_present_notify(); b.present().unwrap(); }, _ => {} } }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let event_loop = EventLoop::builder().build()?;
+    let context = softbuffer::Context::new(event_loop.owned_display_handle())?;
+    let mut surface = None::<softbuffer::Surface<_, _>>;
+
+    #[allow(deprecated)]
+    event_loop.run(move |ev, elwt| {
+        if surface.is_none() {
+            let w = elwt
+                .create_window(
+                    WindowAttributes::default()
+                        .with_title("wlgrid")
+                        .with_inner_size(winit::dpi::LogicalSize::new(400.0, 300.0)),
+                )
+                .unwrap();
+            let (width, height) = (w.inner_size().width, w.inner_size().height);
+            let mut s = softbuffer::Surface::new(&context, w).unwrap();
+            if let (Some(w), Some(h)) = (NonZeroU32::new(width), NonZeroU32::new(height)) {
+                let _ = s.resize(w, h);
+            }
+            s.window().request_redraw();
+            surface = Some(s);
+        }
+
+        let s = surface.as_mut().unwrap();
+        if let Event::WindowEvent { window_id, event } = ev {
+            if window_id != s.window().id() {
+                return;
+            }
+            match event {
+                WindowEvent::Resized(sz) => {
+                    if let (Some(w), Some(h)) =
+                        (NonZeroU32::new(sz.width), NonZeroU32::new(sz.height))
+                    {
+                        let _ = s.resize(w, h);
+                    }
+                }
+                WindowEvent::RedrawRequested => {
+                    if let Ok(mut buf) = s.buffer_mut() {
+                        let w = buf.width().get() as usize;
+                        for (i, p) in buf.iter_mut().enumerate() {
+                            let x = (i % w) as u8;
+                            let y = (i / w) as u8;
+                            *p = 0u32 << 24 | (x as u32) << 16 | (y as u32) << 8 | 180u32;
+                        }
+                        let _ = buf.present();
+                    }
+                }
+                WindowEvent::CloseRequested => elwt.exit(),
+                _ => {}
+            }
+        }
+    })?;
+    Ok(())
 }
-
-fn main() { let mut a = App { c: None, w: None, s: None }; EventLoop::new().unwrap().run_app(&mut a).unwrap(); }
